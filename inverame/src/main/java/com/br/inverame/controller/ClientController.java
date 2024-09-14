e4f4395728098bcfa2bd6089d830a24fc6a1629a
ThailanSousa <joaot.sousa@unifacol.edu.br>
2024-09-14 01:00:44 -0300

    melhorias

--- inverame/src/main/java/com/br/inverame/controller/ClientController.java
+++ inverame/src/main/java/com/br/inverame/controller/ClientController.java
@@ -2,6 +2,7 @@ package com.br.inverame.controller;
 
 import java.util.List;
 import java.util.Optional;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -9,6 +10,7 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -42,6 +44,24 @@ public class ClientController {
             return ResponseEntity.ok(savedClient);
         } catch (Exception e){
             return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(null);
+        }
+    }
+
+     @PutMapping("/{id}") // Adicionar este endpoint
+    public ResponseEntity<Client> updateClient(@PathVariable Long id, @RequestBody Client client) {
+        Optional<Client> existingClient = clientService.findById(id);
+        
+        if (existingClient.isPresent()) {
+            Client updatedClient = existingClient.get();
+            updatedClient.setName(client.getName());
+            updatedClient.setPhone(client.getPhone());
+            updatedClient.setRegisterDate(client.getRegisterDate());
+            
+            // Presumindo que o método updateClient no ClientService realiza a atualização
+            Client savedClient = clientService.updateClient(updatedClient);
+            return ResponseEntity.ok(savedClient);
+        } else {
+            return ResponseEntity.notFound().build();
         }
     }
 
