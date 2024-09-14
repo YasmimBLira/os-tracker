68f1036970cd6ad2b45de057a3be1fd7f87ea8ab
ThailanSousa <joaot.sousa@unifacol.edu.br>
2024-09-13 21:14:32 -0300

    inicio de tudo

---
+++ inverame/src/main/java/com/br/inverame/InverameApplication.java
@@ -0,0 +1,13 @@
+package com.br.inverame;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InverameApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InverameApplication.class, args);
+	}
+
+}
---
+++ inverame/src/test/java/com/br/inverame/InverameApplicationTests.java
@@ -0,0 +1,13 @@
+package com.br.inverame;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InverameApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
