a0603f4599a042cb4715644f3c2460bc6b1c6ba3
2002Larissa <larissadssantos1515@gmail.com>
2024-09-13 22:35:33 -0300

    Corrigindo erro no service adicionando método no repository

--- inverame/src/main/java/com/br/inverame/repository/ClientRepository.java
+++ inverame/src/main/java/com/br/inverame/repository/ClientRepository.java
@@ -1,8 +1,12 @@
 package com.br.inverame.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import com.br.inverame.model.entity.Client;
 
 public interface ClientRepository extends JpaRepository<Client, Long> {
+
+    List<Client> findAll();
     
 }
