f2ab57d93bf75cee3a2f2379f9c0e264615b190e
ThailanSousa <joaot.sousa@unifacol.edu.br>
2024-10-19 22:31:40 -0300

    V1 DO BANCO

--- inverame/src/main/resources/db/migration/V1__create_table_clients.sql
+++ inverame/src/main/resources/db/migration/V1__create_table_clients.sql
@@ -1,8 +1,12 @@
 CREATE TABLE clients (
-  id SERIAL PRIMARY KEY,
-  cnpj VARCHAR(255) NOT NULL UNIQUE,
-  cod_client VARCHAR(255) NOT NULL UNIQUE,
-  name VARCHAR(255) NOT NULL UNIQUE,
-  phone VARCHAR(255),
-  register_date TIMESTAMP NOT NULL
+  id BIGINT GENERATED ALWAYS AS IDENTITY, -- Geração automática de IDs
+  cnpj VARCHAR(20) NOT NULL,
+  cod_client VARCHAR(10) NOT NULL,
+  name VARCHAR(100) NOT NULL,
+  phone VARCHAR(25),
+  register_date TIMESTAMP(6) NOT NULL, -- Corrige o tipo de data/tempo para PostgreSQL
+  PRIMARY KEY (id),  -- 'id' como chave primária é suficiente
+  UNIQUE (cnpj),             -- CNPJ deve ser único
+  UNIQUE (cod_client),       -- Código do cliente deve ser único
+  UNIQUE (name)              -- Nome do cliente deve ser único
 );
